f2563986a8c5d052808fa28c71b589af569610b8
BwizLabs <brandon.r.russell.1@gmail.com>
2019-06-18 19:45:00 -0400

    first commit

---
+++ src/main/kotlin/app/bwizlabs/kotlingbigo/KotlingbigoApplication.kt
@@ -0,0 +1,11 @@
+package app.bwizlabs.kotlingbigo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlingbigoApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlingbigoApplication>(*args)
+}
---
+++ src/test/kotlin/app/bwizlabs/kotlingbigo/KotlingbigoApplicationTests.kt
@@ -0,0 +1,16 @@
+package app.bwizlabs.kotlingbigo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlingbigoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
